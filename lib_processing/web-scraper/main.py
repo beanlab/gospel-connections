03c74e9a4cd7a2933ba0c1e26c22a32800b15b03
ggemre <gmoore1@byu.edu>
2023-10-12 20:10:15 -0600

    Generate first conference text

--- lib_processing/web-scraper/main.py
+++ lib_processing/web-scraper/main.py
@@ -3,27 +3,50 @@ import os
 import scraper
 import unidecode
 
-# Read the JSON file
-with open('std-works-spec.json') as f:
-    data = json.load(f)
+def generate_scripture_text():
 
-# Get the verses for each chapter
-for standard_work, standard_work_data in data['standard_works'].items():
-    standard_work_code = standard_work_data['code']
-    for book_data in standard_work_data['books']:
-        book_code = book_data['code']
-        chapters = book_data['chapters']
-        for chapter in range(1, chapters + 1):
-            print(standard_work + ' ' + book_code + ' ' + str(chapter))
-            scripture = scraper.scrape(standard_work_code, book_code, str(chapter))
-            scripture = unidecode.unidecode(scripture)
+    # Read the JSON file
+    with open('std-works-spec.json') as f:
+        data = json.load(f)
 
-            if scripture == '':
-                print('ERROR 404: ' + standard_work + ' ' + book_code + ' ' + str(chapter))
+    # Get the verses for each chapter
+    for standard_work, standard_work_data in data['standard_works'].items():
+        standard_work_code = standard_work_data['code']
+        for book_data in standard_work_data['books']:
+            book_code = book_data['code']
+            chapters = book_data['chapters']
+            for chapter in range(1, chapters + 1):
+                print(standard_work + ' ' + book_code + ' ' + str(chapter))
+                scripture = scraper.scrape_scriptures(standard_work_code, book_code, str(chapter))
+                scripture = unidecode.unidecode(scripture)
 
-            directory = '../data/scriptures/' + standard_work + '/text/' + book_code
-            if not os.path.exists(directory):
-                os.makedirs(directory)
-            with open(directory + '/' + "{:03d}".format(chapter) + '.txt', 'w') as f:
-                f.write(scripture)
+                if scripture == '':
+                    print('ERROR 404: ' + standard_work + ' ' + book_code + ' ' + str(chapter))
+                    continue
 
+                directory = '../../data/text/scriptures/' + standard_work + '/' + book_code
+                if not os.path.exists(directory):
+                    os.makedirs(directory)
+                with open(directory + '/' + "{:03d}".format(chapter) + '.txt', 'w') as f:
+                    f.write(scripture)
+
+def generate_conference_text(year, month):
+    # get all the sessions for year/month
+    sessions = scraper.get_conference_sessions(year, month)
+    print(sessions)
+    for session in sessions:
+        print(session)
+        conference = scraper.scrape_conference(year, month, session)
+        conference = unidecode.unidecode(conference)
+
+        if conference == '':
+            print('ERROR 404: ' + str(year) + ' ' + str(month) + ' ' + session)
+            continue
+
+        directory = '../../data/text/conference/' + str(year) + '_' + str(month)
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        with open(directory + '/' + session + '.txt', 'w') as f:
+            f.write(conference)
+
+generate_conference_text(2023, 10)
--- lib_processing/web-scraper/scraper.py
+++ lib_processing/web-scraper/scraper.py
@@ -2,14 +2,20 @@ import requests
 from bs4 import BeautifulSoup
 
 # Get the HTML from the URL when given a book of scripture and chapter number
-def get_html(text, book, chapter):
+def get_scripture_html(text, book, chapter):
     url = 'https://www.churchofjesuschrist.org/study/scriptures/' + text + '/' + book + '/' + chapter + '?lang=eng'
     response = requests.get(url)
     response.encoding = 'utf-8'
     return response.text
 
+def get_conference_html(year, month, session):
+    url = 'https://www.churchofjesuschrist.org/study/general-conference/' + str(year) + '/' + str(month) + '/' + str(session) + '?lang=eng'
+    response = requests.get(url)
+    response.encoding = 'utf-8'
+    return response.text
+
 # Get the scripture verses from the HTML
-def get_scripture(html):
+def get_scripture_text(html):
     soup = BeautifulSoup(html, 'html.parser')
     verses = soup.find_all('p', class_='verse')
     scripture = ''
@@ -29,14 +35,61 @@ def get_scripture(html):
         scripture += verse_text + '\n'
     return scripture
 
+def get_conference_text(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    paragraphs = soup.find_all('p')
+    conference = ''
+    # go through each paragraph where id is p# where # is a number
+    for paragraph in paragraphs:
+        p_id = paragraph.get('id')
+        if p_id is not None and p_id[0] == 'p':
+            # get rid of all <sup> elements, (references)
+            for sup in paragraph.find_all('sup'):
+                sup.decompose()
+
+            # post processing
+            paragraph_text = ' '.join(paragraph.text.split())
+
+            conference += paragraph_text + '\n'
+    return conference
+
 # Main function that prints verses given a book of scripture and chapter number
-def scrape(text, book, chapter):
-    html = get_html(text, book, chapter)
-    scripture = get_scripture(html)
+def scrape_scriptures(text, book, chapter):
+    html = get_scripture_html(text, book, chapter)
+    scripture = get_scripture_text(html)
 
     if scripture[-1] == '\n':
         scripture = scripture[:-1]
 
     return scripture
 
+def get_conference_sessions(year, month):
+    sitemap_urls = []
+    sessions = []
+    # read xml file
+    url = "https://sitemaps.churchofjesuschrist.org/sitemap-service/www.churchofjesuschrist.org/en/index.xml"
+    response = requests.get(url)
+    response.encoding = "utf-8"
+    soup = BeautifulSoup(response.text, features="xml")
+    for loc in soup.find_all("loc"):
+        sitemap_urls.append(loc.text)
+    for sitemap_url in sitemap_urls:
+        url = sitemap_url
+        response = requests.get(url)
+        response.encoding = "utf-8"
+        soup = BeautifulSoup(response.text, features="xml")
+        for loc in soup.find_all("loc"):
+            if loc.text.startswith("https://www.churchofjesuschrist.org/study/general-conference/" + str(year) + '/' + str(month)):
+                sessions.append((loc.text.split('/')[-1]).split('?')[0])
 
+    return sessions
+
+
+def scrape_conference(year, month, session):
+    html = get_conference_html(year, month, session)
+    conference = get_conference_text(html)
+
+    if conference != '' and conference[-1] == '\n':
+        conference = conference[:-1]
+
+    return conference
